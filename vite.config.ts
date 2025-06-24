b497e5059506b6fe341bf50bf1c966530c8db1da
garvithindoliya16 <garvithindoliya16@gmail.com>
2025-06-24 14:34:54 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/garvitWork/",
   server: {
     host: "::",
     port: 8080,
